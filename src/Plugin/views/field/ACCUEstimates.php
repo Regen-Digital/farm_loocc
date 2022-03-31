06a8d3f74690416cbd7541fe3adab7047da81899
Paul Weidner <paul.weidner@gmail.com>
2022-03-30 17:49:16 -0700

    Change the ACCUEstimates field to not extend PrerenderList.

--- src/Plugin/views/field/ACCUEstimates.php
+++ src/Plugin/views/field/ACCUEstimates.php
@@ -3,7 +3,7 @@
 namespace Drupal\farm_loocc\Plugin\views\field;
 
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
-use Drupal\views\Plugin\views\field\PrerenderList;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 use Drupal\views\ViewExecutable;
 
@@ -12,7 +12,7 @@ use Drupal\views\ViewExecutable;
  *
  * @ViewsField("farm_loocc_accu_estimates")
  */
-class ACCUEstimates extends PrerenderList {
+class ACCUEstimates extends FieldPluginBase {
 
   /**
    * {@inheritdoc}
@@ -32,42 +32,29 @@ class ACCUEstimates extends PrerenderList {
   /**
    * {@inheritdoc}
    */
-  public function renderItems($items) {
+  protected function allowAdvancedRender() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    // Get estimates for the row.
+    $estimate_id = $values->farm_loocc_estimate_id;
+    $estimates = $this->accuEstimates[$estimate_id] ?? [];
+
+    // Get options.
+    $method_ids = array_column($estimates, 'method_id');
+    $method_names = array_column($estimates, 'method_name');
+
     $render = [
       '#type' => 'select',
       '#title' => NULL,
-      '#options' => $items,
+      '#options' => array_combine($method_ids, $method_names),
     ];
     return \Drupal::service('renderer')->render($render);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function render_item($count, $item) { // phpcs:ignore
-    /** @var \Drupal\farm_loocc\LooccEstimateInterface $looc_estimate */
-    $looc_estimate = \Drupal::service('farm_loocc.estimate');
-
-    // Estimate values.
-    $method_id = $item['method_id'];
-    $annual = $item['annual'];
-    $project = $item['project'];
-
-    // Render the estimate with an improved name, if possible.
-    $method_name = $method_id;
-    if ($erf_cobenefits = $looc_estimate->getErfCobenefits($method_id)) {
-      $method_name = $erf_cobenefits['methodName'];
-    }
-    return "$method_name : $annual ($project)";
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getItems(ResultRow $values) {
-    $id = $values->farm_loocc_estimate_id;
-    $estimates = $this->accuEstimates[$id] ?? [];
-    return $estimates;
   }
 
   /**
@@ -78,6 +65,9 @@ class ACCUEstimates extends PrerenderList {
    */
   protected function getAccuEstimates(): array {
 
+    /** @var \Drupal\farm_loocc\LooccEstimateInterface $looc_estimate */
+    $looc_estimate = \Drupal::service('farm_loocc.estimate');
+
     // Query all accu estimates.
     $accu_estimates = \Drupal::database()->select('farm_loocc_accu_estimate', 'flae')
       ->fields('flae', ['estimate_id', 'method_id', 'annual', 'project', 'warning_message'])
@@ -87,8 +77,21 @@ class ACCUEstimates extends PrerenderList {
     // Map each accu estimate to the estimate id.
     $all_estimates = [];
     foreach ($accu_estimates as $result) {
-      $id = $result->estimate_id;
-      $all_estimates[$id][] = (array) $result;
+
+      // Estimate values.
+      $estimate = (array) $result;
+      $method_id = $estimate['method_id'];
+
+      // Render the estimate with an improved name, if possible.
+      $method_name = $method_id;
+      if ($erf_cobenefits = $looc_estimate->getErfCobenefits($method_id)) {
+        $method_name = $erf_cobenefits['methodName'];
+      }
+      $estimate['method_name'] = $method_name;
+
+      // Include the estimate.
+      $estimate_id = $result->estimate_id;
+      $all_estimates[$estimate_id][] = $estimate;
     }
 
     return $all_estimates;
